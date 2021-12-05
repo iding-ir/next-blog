b37aa11f5891d08208d474fadee040291ded3b84
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-05 11:23:09 +0330

    Fix create function

--- utils/createPost.ts
+++ utils/createPost.ts
@@ -4,6 +4,9 @@ const createPost = async (data: any) => {
   const res = await fetch(`${api}`, {
     method: "POST",
     body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
   });
 
   const post = await res.json();
